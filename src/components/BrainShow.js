32be74bb789d7efcac77b0fff5728a67d1b9df03
Lenny Thomas <105914077+LennyThomasLT@users.noreply.github.com>
2023-04-26 01:05:02 +0700

    pinky

--- src/components/BrainShow.js
+++ src/components/BrainShow.js
@@ -4,7 +4,13 @@ function BrainShow() {
   return (
     <div
       className="container-fluid"
-      style={{ padding: "0em", backgroundColor: "#00000" }}
+      style={{
+        padding: "0em",
+        backgroundColor: "#00000",
+        position: "fixed",
+        overflow: "hidden",
+        minwidth: "100%",
+      }}
     >
       <video src={video} width="100%" height="100%" autoPlay muted></video>
     </div>
